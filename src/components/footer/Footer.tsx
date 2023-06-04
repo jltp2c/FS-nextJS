b1f909d8eb6138639ca8debdeed1d3f90e7be08b
Jean-Luc <103611121+jltp2c@users.noreply.github.com>
2023-06-04 23:39:57 +0900

    css

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import styles from './page.module.css'
+import Image from 'next/image';
 
 function Footer() {
   return (
     <div className={styles.container}>
         <div> ©2023 JLT. All rights reserved.</div>
-        <div>
-            
+        <div className={styles.imgContainer}>
+            <Image src="/1.png" width={15} height={15} alt="jlt img fb"/>
+            <Image src="/2.png" width={15} height={15} alt="jlt img insta"/>
+            <Image src="/3.png" width={15} height={15} alt="jlt img twitter"/>
+            <Image src="/4.png" width={15} height={15} alt="jlt img youtube"/>            
         </div>
     </div>
   )
