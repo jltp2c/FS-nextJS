75e5fb71f234ede0200c1a49c07cf1aceb66bfbd
Jean-Luc <103611121+jltp2c@users.noreply.github.com>
2023-06-16 12:19:51 +0900

    mongoose

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 
 
 async function getData() {
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts', { cache: 'no-store'  });
+  const res = await fetch('http://localhost:3000/api/posts', { cache: 'no-store'  });
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
