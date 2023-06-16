40432143e92cb3dcc5199b2acbb90307b4aa7cd3
Jean-Luc <103611121+jltp2c@users.noreply.github.com>
2023-06-16 23:50:26 +0900

    rm env

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -1,16 +1,10 @@
 import connect from "@/utils/db.js";
-import Post from "@/models/Post.js";
 import { NextResponse } from "next/server";
-import { stringify } from "querystring";
 
 export const GET = async (request) => {
   try {
     await connect();
-    const posts = await Post.find();
-    return new NextResponse(JSON.stringify(posts), {
-      status: 200,
-    });
   } catch (error) {
-    return new NextResponse("connection failed with database", { status: 500 });
+    return NextResponse("database error", { status: 500 });
   }
 };
