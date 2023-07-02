9350126a70121ad490255b299eddf52867d71c26
Jean-Luc <103611121+jltp2c@users.noreply.github.com>
2023-07-02 19:53:17 +0800

    auth credentials

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,8 @@
+import { connect } from "http2";
 import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
+import User from "@/models/User.js";
 
 const handler = NextAuth({
   providers: [
@@ -7,7 +10,35 @@ const handler = NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      async authorize(credentials) {
+        await connect();
+        try {
+          const user = await User.findOne({ email: credentials.email });
+          if (user) {
+            const passwordCorrect = await bcrypt.compare(
+              credentials.password,
+              user.password
+            );
+            if (passwordCorrect) {
+              return user;
+            } else {
+              throw new Error("your credential is not found");
+            }
+          } else {
+            throw new Error("User not found ");
+          }
+        } catch (error) {
+          throw new Error(error);
+        }
+      },
+    }),
   ],
+  pages: {
+    error: "/dashboard/login",
+  },
 });
 
 export { handler as GET, handler as POST };
---
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -0,0 +1,30 @@
+"use client"
+import { signIn } from 'next-auth/react'
+import React from 'react'
+import styles from "./page.module.css"
+
+function Login() {
+
+ 
+  const handleSubmit = async (e) => {
+  e.preventDefault()
+  
+    const email = e.target[0].value
+    const password = e.target[1].value
+
+    signIn('credentials', {email , password})
+  };
+
+  return (
+    <div className={styles.container} onSubmit={handleSubmit}>
+      <form className={styles.form} >
+        <input type="email" placeholder='email' className={styles.input} required/>
+        <input type="password" placeholder='password' className={styles.input} required/>
+        <button className={styles.button}>Login</button>
+      </form>
+      <button onClick={()=>signIn("google")}> Sign in with Google</button>
+    </div>
+  )
+}
+
+export default Login
